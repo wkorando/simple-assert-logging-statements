9a72ddc7350e123d902f38b500d0d0da537acf6b
Billy Korando <wkorando@gmail.com>
2019-04-16 16:10:38 -0500

    polish

--- src/test/java/com/ibm/developer/TestLogProducingService.java
+++ src/test/java/com/ibm/developer/TestLogProducingService.java
@@ -5,8 +5,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import com.ibm.developer.LogProducingService;
-
 public class TestLogProducingService {
 	private LogProducingService service = new LogProducingService();
 
